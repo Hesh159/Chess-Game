b458826eb5a5b21e5bf4be5e11f640dae0d66505
Hession <darragh.hession@embl.de>
2023-04-25 20:21:13 +0200

    #1 - Add buildChessboard method to Board

--- src/main/java/com/hession/chessapp/board/Board.java
+++ src/main/java/com/hession/chessapp/board/Board.java
@@ -3,7 +3,7 @@ package com.hession.chessapp.board;
 import javafx.scene.layout.GridPane;
 
 public class Board {
-
+    private final String color = "Black";
     private final GridPane chessBoard = new GridPane();
 
     public Board() {
@@ -19,9 +19,30 @@ public class Board {
         for (int row = 1; row < 9; row++) {
             for (int col = 1; col < 9; col++) {
                 ChessSquare chessSquare = new ChessSquare();
-                chessSquare.setSquareColor(col, row);
-                chessBoard.add(chessSquare, col, row);
+                addChessSquare(chessSquare, row, col);
+
+                if (col == 1) {
+                    chessSquare.addNumberToSquareView();
+                }
+
+                if (row == 1) {
+                    chessSquare.addLetterToSquareView();
+                }
             }
         }
+
+        if (color == "Black") {
+            chessBoard.setRotate(180);
+        }
+    }
+
+    private void addChessSquare(ChessSquare chessSquare, int row, int col) {
+        chessSquare.setRow(row);
+        chessSquare.setCol(col);
+        chessSquare.setSquareColor();
+        chessSquare.createAndSetId();
+        chessBoard.add(chessSquare, col, row);
+
+        System.out.println("id: " + chessSquare.getId() + " row: " + chessSquare.getRow() + " col: " + chessSquare.getCol());
     }
 }
