a22be98bd929b5235ec9d012d4ecb52478af7939
Hession <darragh.hession@embl.de>
2023-04-25 20:22:20 +0200

    #1 - Add createAndSetId method to ChessSquare

--- src/main/java/com/hession/chessapp/board/ChessSquare.java
+++ src/main/java/com/hession/chessapp/board/ChessSquare.java
@@ -2,17 +2,37 @@ package com.hession.chessapp.board;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class ChessSquare extends Rectangle {
-
+    private static final int ROW_START_INT = 9;
+    private static final int ASCII_LOWER_LETTER_START = 96;
     private final Color light = Color.BEIGE;
     private final Color dark = Color.BROWN;
 
     private boolean isOccupied;
+    private int row;
+    private int col;
 
     public ChessSquare() {
         this.setWidth(50);
         this.setHeight(50);
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getCol() {
+        return this.col;
     }
 
 
@@ -33,13 +53,10 @@ public class ChessSquare extends Rectangle {
     }
 
     /**
-     * @param col - Column of Square
-     * @param row - Row of Square
-     *
      * Determines and sets colour of Square - on a chessboard if a squares row and column are both odd, or both even numbers,
      * the square is dark. As our grid starts from the top rather than the bottom like a chessboard, the square will be light
      */
-    public void setSquareColor(int col, int row) {
+    public void setSquareColor() {
         boolean isColEven = (col % 2 == 0);
         boolean isRowEven = (row % 2 == 0);
 
@@ -50,4 +67,29 @@ public class ChessSquare extends Rectangle {
         }
     }
 
+    public void createAndSetId() {
+        char letterCoordinate = getLetterCoordinate();
+        int numberCoordinate = getNumberCoordinate();
+        String letterCoordinateString = Character.toString(letterCoordinate);
+        String numberCoordinateString = Integer.toString(numberCoordinate);
+        setId(letterCoordinateString + numberCoordinateString);
+    }
+
+    private char getLetterCoordinate() {
+        return (char) (col + ASCII_LOWER_LETTER_START);
+    }
+
+    private int getNumberCoordinate() {
+        return ROW_START_INT - row;
+    }
+
+    public void addNumberToSquareView() {
+        int numberCoordinate = getNumberCoordinate();
+        Text numberText = new Text(Integer.toString(numberCoordinate));
+    }
+
+    public void addLetterToSquareView() {
+        char letterCoordinate = getLetterCoordinate();
+        Text letterText = new Text(Character.toString(letterCoordinate));
+    }
 }
