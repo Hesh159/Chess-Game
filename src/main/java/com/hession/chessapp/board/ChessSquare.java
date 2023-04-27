cb1a5af3a70078c3c5d4aaf0da931365bd336c3a
Hession <darragh.hession@embl.de>
2023-04-27 17:12:06 +0200

    #1 - Add javadocs to ChessSquare

--- src/main/java/com/hession/chessapp/board/ChessSquare.java
+++ src/main/java/com/hession/chessapp/board/ChessSquare.java
@@ -7,8 +7,8 @@ import javafx.scene.text.Text;
 public class ChessSquare extends Rectangle {
     private static final int ROW_START_INT = 9;
     private static final int ASCII_LOWER_LETTER_START = 96;
-    private final Color light = Color.BEIGE;
-    private final Color dark = Color.BROWN;
+    private static final Color LIGHT = Color.BEIGE;
+    private static final Color DARK = Color.BROWN;
 
     private boolean isOccupied;
     private int row;
@@ -19,18 +19,38 @@ public class ChessSquare extends Rectangle {
         this.setHeight(50);
     }
 
+    /**
+     * Set the row of the ChessSquare
+     *
+     * @param row - The grid row of the ChessSquare
+     */
     public void setRow(int row) {
         this.row = row;
     }
 
+    /**
+     * Retrieve the row of the ChessSquare
+     *
+     * @return - The grid row the ChessSquare is placed on
+     */
     public int getRow() {
         return this.row;
     }
 
+    /**
+     * Set the column of the ChessSquare
+     *
+     * @param col - The grid column of the ChessSquare
+     */
     public void setCol(int col) {
         this.col = col;
     }
 
+    /**
+     * Retrieve the column of the ChessSquare
+     *
+     * @return - The grid column the ChessSquare is placed on
+     */
     public int getCol() {
         return this.col;
     }
@@ -44,9 +64,9 @@ public class ChessSquare extends Rectangle {
     }
 
     /**
-     * @param occupationStatus - Whether a square has a piece on it
-     *
      * Sets the Squares isOccupied property to occupationStatus
+     *
+     * @param occupationStatus - Whether a square has a piece on it
      */
     public void setSquareOccupied(boolean occupationStatus) {
         this.isOccupied = occupationStatus;
@@ -54,19 +74,25 @@ public class ChessSquare extends Rectangle {
 
     /**
      * Determines and sets colour of Square - on a chessboard if a squares row and column are both odd, or both even numbers,
-     * the square is dark. As our grid starts from the top rather than the bottom like a chessboard, the square will be light
+     * the square is dark. As our grid's (1, 1) coordinate is in the top left, rather than bottom left like a chessboard, a square
+     * with odd/even rows and cols should be set to light
      */
     public void setSquareColor() {
         boolean isColEven = (col % 2 == 0);
         boolean isRowEven = (row % 2 == 0);
 
         if (isRowEven == isColEven) {
-            this.setFill(light);
+            this.setFill(LIGHT);
         } else {
-            this.setFill(dark);
+            this.setFill(DARK);
         }
     }
 
+    /**
+     * Create and set the ID of the ChessSquare - the id component represents the coordinate of the ChessSquare.
+     * The letter coordinate is retrieved from the column of the ChessSquare, while the number coordinate is retrieved
+     * from the row of the ChessSquare
+     */
     public void createAndSetId() {
         char letterCoordinate = getLetterCoordinate();
         int numberCoordinate = getNumberCoordinate();
@@ -75,10 +101,23 @@ public class ChessSquare extends Rectangle {
         setId(letterCoordinateString + numberCoordinateString);
     }
 
+    /**
+     * Determine the letter coordinate of the ChessSquare, the value of the column is added to ASCII_LOWER_LETTER_START. The value of ASCII_LOWER_LETTER_START
+     * is 96 - one before the Ascii value of 'a', as the column value ranges from 1 to 8, adding the column to 96 returns the characters between 'a' and 'h' -
+     * the letter coordinates of a chessboard
+     *
+     * @return - The letter coordinate of the ChessSquare, as a char
+     */
     private char getLetterCoordinate() {
         return (char) (col + ASCII_LOWER_LETTER_START);
     }
 
+    /**
+     * Determine the number coordinate of the ChessSquare. The value of ROW_START_INT is set to 9, taking away the value of row returns the correct row value of the square.
+     * This is the case as the top left square of the javafx grid has a coordinate of (1,1), whereas the top left square of a chessboard has a coordinate of (1,8)
+     *
+     * @return - The number coordinate of the ChessSquare
+     */
     private int getNumberCoordinate() {
         return ROW_START_INT - row;
     }
