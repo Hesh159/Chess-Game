7ef2f77f2ff6032b41753fd4ddf6f8f4ec9b70f4
Hession <darragh.hession@embl.de>
2023-04-27 16:53:39 +0200

    #1 - ChessSquare now wraps into a StackPane container

--- src/main/java/com/hession/chessapp/board/Board.java
+++ src/main/java/com/hession/chessapp/board/Board.java
@@ -1,6 +1,7 @@
 package com.hession.chessapp.board;
 
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
 public class Board {
     private final String color = "Black";
@@ -18,16 +19,8 @@ public class Board {
 
         for (int row = 1; row < 9; row++) {
             for (int col = 1; col < 9; col++) {
-                ChessSquare chessSquare = new ChessSquare();
-                addChessSquare(chessSquare, row, col);
-
-                if (col == 1) {
-                    chessSquare.addNumberToSquareView();
-                }
-
-                if (row == 1) {
-                    chessSquare.addLetterToSquareView();
-                }
+                StackPane chessSquareContainer = new StackPane();
+                addChessSquareContainerToBoard(chessSquareContainer, row, col);
             }
         }
 
@@ -36,13 +29,25 @@ public class Board {
         }
     }
 
-    private void addChessSquare(ChessSquare chessSquare, int row, int col) {
+    private void addChessSquareContainerToBoard(StackPane chessSquareContainer, int row, int col) {
+        ChessSquare chessSquare = new ChessSquare();
+        setChessSquareAttributes(chessSquare, row, col);
+        chessSquareContainer.getChildren().add(chessSquare);
+        chessBoard.add(chessSquareContainer, col, row);
+
+        if (col == 1) {
+            chessSquare.addNumberToSquareView();
+        }
+        if (row == 1) {
+            chessSquare.addLetterToSquareView();
+        }
+        System.out.println("id: " + chessSquare.getId() + " row: " + chessSquare.getRow() + " col: " + chessSquare.getCol());
+    }
+
+    private void setChessSquareAttributes(ChessSquare chessSquare, int row, int col) {
         chessSquare.setRow(row);
         chessSquare.setCol(col);
         chessSquare.setSquareColor();
         chessSquare.createAndSetId();
-        chessBoard.add(chessSquare, col, row);
-
-        System.out.println("id: " + chessSquare.getId() + " row: " + chessSquare.getRow() + " col: " + chessSquare.getCol());
     }
 }
